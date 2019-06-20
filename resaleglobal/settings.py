aae1009fbc67413dd8c294cb19e537036521ef18
j3steveloper <selkasse@gmu.edu>
2019-06-20 18:36:36 -0400

    makemigrations on initial build and add templates section to settings.py

--- resaleglobal/settings.py
+++ resaleglobal/settings.py
@@ -52,6 +52,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'resaleglobal.urls'
 
 TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'resaleglobal.wsgi.application'

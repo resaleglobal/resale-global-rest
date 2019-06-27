4e8934bca7ee60e1d1d8a877dfcb1734799e01a8
Kevyn Hale <kevyn@kevynhale.com>
2019-06-26 20:30:07 -0600

    initial setup of categories

--- resaleglobal/reseller/models.py
+++ resaleglobal/reseller/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from resaleglobal.account.models import Reseller, Consignor
 
-# Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+
+class Merchandise(models.Model):
+  # TODO ADD DEFAULT MERCHANDISE FIELDS
+  name = models.CharField(max_length=50, unique=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Field(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+
+class MerchandiseFields(models.Model):
+  field = models.ForeignKey(Field, on_delete=models.CASCADE)
+  merchandise = models
+
+class Brand(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+
+class Item(models.Model):
+  reseller = models.ForeignKey(Reseller, on_delete=models.CASCADE)
+  consignor = models.ForeignKey(Consignor, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50)
+  brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+  price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class ItemFields(models.Model):
+  value = models.CharField(max_length=255)
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  field = models.ForeignKey(Field, on_delete=models.CASCADE)

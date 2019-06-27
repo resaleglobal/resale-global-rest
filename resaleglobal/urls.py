a74e608c2351fea23745d946aae89dfe79c44aa0
Kevyn Hale <kevyn@kevynhale.com>
2019-06-26 19:48:23 -0600

    update general apis

--- resaleglobal/urls.py
+++ resaleglobal/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('account/v1/', include('resaleglobal.account.urls')),
-    path('admin/v1/<str:accountId>/', include('resaleglobal.admin.urls'))
+    path('admin/v1/<str:accountId>/', include('resaleglobal.admin.urls')),
+    path('reseller/v1/<str:accountId>/', include('resaleglobal.reseller.urls')),
+    path('consignor/v1/<str:accountId>/', include('resaleglobal.consignor.urls')),
+    path('buyer/v1/', include('resaleglobal.buyer.urls'))
 ]
